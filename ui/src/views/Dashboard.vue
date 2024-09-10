9378b207a0d42a4bed568ce7bff3ac8a0fa2e29e
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-09-10 12:09:42 +0200

    refactor: Simplify migration status check in Dashboard.vue

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1166,7 +1166,7 @@ export default {
       console.log("this apps migrated ", this.apps.some((app) => app.status === "migrated"));
       console.log("account-provide",this.apps.some((app) => app.id === "account-provider"));
       console.log(this.apps.some((app) => app.status === "migrated") && !this.apps.some((app) => app.id === "account-provider"));
-      return this.apps.some((app) => app.status === "migrated") && !this.apps.some((app) => app.id === "account-provider");
+      return !this.apps.some((app) => app.id === "account-provider");
     },
     emailApp() {
       return this.apps.find((app) => app.id === "nethserver-mail");
